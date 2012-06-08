d928b6942c1ef1e0be36d53fb111ee8f6c99132c
Leonard Wörteler <leo@woerteler.de>
2012-06-08 15:39:26 +0200

    Mavenized project.

---
+++ src/test/java/infovis/data/test/BusTimeTests.java
@@ -0,0 +1,10 @@
+package infovis.data.test;
+
+/**
+ * Tests for the time-based algorithms in {@link infovis.data.BusTime}.
+ * 
+ * @author Leo Woerteler
+ */
+public class BusTimeTests {
+  // TODO
+}
