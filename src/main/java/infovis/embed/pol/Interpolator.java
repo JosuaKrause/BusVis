f008df0c4fff0a5b4524eb0eb37714ba4e0cb015
Leonard Wörteler <leo@woerteler.de>
2012-06-23 10:50:43 +0200

    Made concrete Interpolators anonymous classes.
    As both interpolators are immutable and final, there's never a need for
    more than one instance.

--- src/main/java/infovis/embed/pol/Interpolator.java
+++ src/main/java/infovis/embed/pol/Interpolator.java
@@ -1,7 +1,10 @@
 package infovis.embed.pol;
 
+import static java.lang.Math.*;
+
 /**
- * An interpolator maps values from 0 to 1 to 0 to 1.
+ * An interpolator maps values from the range <code>[0,1]</code> to
+ * <code>[0,1]</code>.
  * 
  * @author Joschi <josua.krause@googlemail.com>
  */
@@ -15,15 +18,21 @@ public interface Interpolator {
    */
   double interpolate(double t);
 
-  /**
-   * Smooth interpolation.
-   */
-  Interpolator SMOOTH = new SinInterpolator();
+  /** Smooth interpolation. */
+  Interpolator SMOOTH = new Interpolator() {
+    @Override
+    public double interpolate(final double t) {
+      return sin((t - 0.5) * PI) * 0.5 + 0.5;
+    }
+  };
 
-  /**
-   * Linear interpolation.
-   */
-  Interpolator LINEAR = new LinearInterpolator();
+  /** Linear interpolation. */
+  Interpolator LINEAR = new Interpolator() {
+    @Override
+    public double interpolate(final double t) {
+      return t;
+    }
+  };
 
   /**
    * The standard animation duration.
