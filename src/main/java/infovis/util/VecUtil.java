94a1bb4618eab5ecc810673367417da189ac6f18
Josua Krause <josua.krause@googlemail.com>
2012-07-01 20:22:00 +0200

    added needed utility methods

--- src/main/java/infovis/util/VecUtil.java
+++ src/main/java/infovis/util/VecUtil.java
@@ -1,5 +1,6 @@
 package infovis.util;
 
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 
 /**
@@ -78,6 +79,41 @@ public final class VecUtil {
    */
   public static Point2D setLength(final Point2D v, final double l) {
     return mulVec(v, l / getLength(v));
+  }
+
+  /**
+   * Rotates a point around the center such that the result has the given
+   * distance to the original point.
+   * 
+   * @param pos The point to rotate.
+   * @param center The center.
+   * @param dist The distance.
+   * @return The point that has the given distance to the original point.
+   * @throws IllegalArgumentException When the distance is longer than the
+   *           diameter.
+   */
+  public static Point2D rotate(final Point2D pos, final Point2D center, final double dist) {
+    final double f = dist > 0 ? 1 : -1;
+    final double dSq = dist * dist;
+    final Point2D rad = subVec(pos, center);
+    final double radSq = getLengthSq(rad);
+    if(dSq > 4 * radSq) throw new IllegalArgumentException("distance too long");
+    return rotateByAngle(pos, center, f * Math.acos(1 - dSq * 0.5 / radSq));
+  }
+
+  /**
+   * Rotates a point a given angle around the center.
+   * 
+   * @param pos The point to rotate.
+   * @param center The center.
+   * @param angle The angle.
+   * @return The rotated point.
+   */
+  public static Point2D rotateByAngle(final Point2D pos, final Point2D center,
+      final double angle) {
+    final AffineTransform at = AffineTransform.getRotateInstance(angle, center.getX(),
+        center.getY());
+    return at.transform(pos, null);
   }
 
 }
